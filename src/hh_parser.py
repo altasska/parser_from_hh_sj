65686195203a95c79d32d8f8c35c3da3ae0e4f2f
Zhanna Popova <zh.denisovna.97@gmail.com>
2023-07-28 20:23:32 +0500

    add a search query for method get_vacancies

--- src/hh_parser.py
+++ src/hh_parser.py
@@ -6,14 +6,20 @@ class JobParserFromHH(AbstractJobParser):
     def connect(self):
         pass
 
-    def get_vacancies(self):
-        url = "https://api.hh.ru/vacancies"
+    def get_vacancies(self, search_query):
+        """
+        здесь просто возвращаем json-файл со всеми вакансиями по запросу
+        """
+        url = f"https://api.hh.ru/vacancies?text={search_query}"
         response = requests.get(url)
 
         if response.status_code == 200:
             data = response.json()
             vacancies = data.get('items', [])
-            return vacancies  # возврат вакансий в json-формате
 
+            if not vacancies:
+                return "Извините, по вашему поисковому запросу не нашлось вакансий."
+
+            return vacancies
         else:
-            return f"Ошибка {response.status_code}"
+            return "Ошибка {response.status_code}"
